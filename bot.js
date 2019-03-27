3fdab83a5d2c30ac0d40c87e9dfd6381e8980d69
pleasela22 <48952303+pleasela22@users.noreply.github.com>
2019-03-27 15:35:45 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("560471430886850560")
+setInterval(function() {
+channel.send(`spamspamsapmspamsspamsapmssapmssapmssapmssapmssapmssapmssapms`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
